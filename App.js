80ce004f02abafc5906d33cca00e2aedf49c0ac8
carolina oliveira <carolina.limaoliveiranovo@gmail.com>
2025-03-04 23:03:38 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+let amigos = [];
+
+function adicionarAmigo(){
+    let nome = document.getElementById("amigo");
+    if(nome.value == ""){
+         alert("Por favor, insira um nome."); alert("Por favor, insira um nome.");
+    }
+    amigos.push(nome.value);
+    nome.value = "";
+    listaDeAmigos();
+}
+
+function listaDeAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    if(amigos.length > 0){
+         amigos.forEach(element => { amigos.forEach(element => {
+            let item = document.createElement("li");
+            item.textContent = element;
+            lista.appendChild(item);
+        });
+    }
+}
+
+function sortearAmigo(){
+    if(!amigos.length){
+         alert("A lista de nomes está vazia! Adicione nomes para realizar o sorteio."); alert("A lista de nomes está vazia! Adicione nomes para realizar o sorteio.");
+        return;
+    }
+    let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    resultado.textContent = amigos[numeroAleatorio];
+}
